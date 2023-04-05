68c66afdea0ba234f0a8d9a950d0db5acdc17df6
RomanTeen1 <fquez716@gmail.com>
2023-04-04 21:32:58 -0700

    completed adding user and id. Completed making sure if user was already added then json req would retrieve user and id

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const bodyParser = require('body-parser');
 require('dotenv').config()
 
 app.use(cors())
@@ -9,8 +10,29 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+const users = [];
+const usersIds = [];
+app.post('/api/users', bodyParser.urlencoded({extended:false}), function(req, res) {
+  const user = req.body.username;
 
+  if(!users.includes(user)){
+    users.push(user);
+    let userId = users.length-1;
+    usersIds.push(userId);
 
+    res.json({user: user, usersIds: userId});
+  }
+  else{
+    getIndexOfUser = users.indexOf(user);
+    let myUser = users[getIndexOfUser];
+
+    res.json({user: myUser, usersIds: getIndexOfUser});
+  }  
+
+  console.log(users);
+  console.log(usersIds);
+  
+});
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
