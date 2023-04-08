2eacb4e2ada52887d492f0add80ce98dcb268e5a
RomanTeen1 <fquez716@gmail.com>
2023-04-07 22:08:40 -0700

    redid project!! Added in a mongodabase -> created schema -> then model -> then tested to see if record can be added. All was successful

--- index.js
+++ index.js
@@ -2,38 +2,52 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const bodyParser = require('body-parser');
-require('dotenv').config()
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+// mongoose connection to the Data Base
+mongoose.connect(process.env.MONGO_URI, console.log('connected to MongoDB succesfull')
+);
+
+//create schema
+const userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    unique: true
+  },
+}, {
+  versionKey: false
+});
+
+//Create model
+const User = mongoose.model('User', userSchema);
 
 app.use(cors())
+app.use(express.urlencoded({extended:false}));
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-const users = [];
-const usersIds = [];
-app.post('/api/users', bodyParser.urlencoded({extended:false}), function(req, res) {
-  const user = req.body.username;
 
-  if(!users.includes(user)){
-    users.push(user);
-    let userId = users.length-1;
-    usersIds.push(userId);
+app.post('/api/users', async (req, res) =>{
+  const username = req.body.username;
 
-    res.json({user: user, usersIds: userId});
-  }
-  else{
-    getIndexOfUser = users.indexOf(user);
-    let myUser = users[getIndexOfUser];
+  const user = await User.create({
+    username
+  });
 
-    res.json({user: myUser, usersIds: getIndexOfUser});
-  }  
 
-  console.log(users);
-  console.log(usersIds);
-  
+  res.json(user);
 });
 
+
+
+// Add exercise to user
+app.post("/api/users/:_id/exercises", bodyParser.urlencoded({extended:false}), (req, res) => {
+  
+  }
+);
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
